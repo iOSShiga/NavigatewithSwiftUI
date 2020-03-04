cea653561e12ccf12d7be1aa125685fe5f8741f8
Shigasuresh@123 <shigasuresh546@gmail.com>
2020-03-04 21:55:37 +0530

    initial

--- NavigatewithSwiftUI/ContentView.swift
+++ NavigatewithSwiftUI/ContentView.swift
@@ -9,9 +9,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var showCategoriesSheet = false
+    
     var body: some View {
+        NavigationView {
+            
+            VStack {
+                NavigationLink(destination: Text("Categories content view"), isActive: $showCategoriesSheet) {
+                    EmptyView()
+                }
+            
+        List {
+            NavigationLink(destination: Text("My Row 1 Content")) {
         Text("Hello, World!")
+            }
+            
+            NavigationLink(destination: Text("My Row 2 Content")) {
+        Text("Hello, World!")
+            }
+            NavigationLink(destination: Text("My Row 3 Content")) {
+        Text("Hello, World!")
+            }
+        }
+    .navigationBarTitle(Text("Medium"))
+        .navigationBarItems(trailing: Button(action: {
+            self.showCategoriesSheet = true
+        }, label: {
+            Text("Categories")
+        }))
+            /*
+            .sheet(isPresented: $showCategoriesSheet) {
+                Text("Categories Sheet content")
+            } */
     }
+            }
+
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
